96712046e76547d18f9eed1ce892cb9f90979a1f
AndreaPellizzari <andrea.pellizzari19024@gmail.com>
2024-11-20 20:57:31 +0100

    commit deck cards

---
+++ flutterscala/lib/pages/home/card.dart
@@ -0,0 +1,7 @@
+import 'package:flutterscala/pages/home/deck.dart';
+
+class Card {
+  String name = "";
+
+  Card({required this.name});
+}
---
+++ flutterscala/lib/pages/home/deck.dart
@@ -0,0 +1,7 @@
+import 'package:flutterscala/pages/home/card.dart';
+
+class Deck {
+  List<Card> cards;
+
+  Deck({required this.cards});
+}
